62da42c3c1eec9759b3fac7c27e566d425f8ee3f
Daniel Damkoehler <dandam@gmail.com>
2018-12-30 11:44:36 +0200

    Ch8: user model & following tests

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
 
     def followed_posts(self):
         followed = Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id).filter(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         return followed.union(self.posts).order_by(Post.timestamp.desc())
 
